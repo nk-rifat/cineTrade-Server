b1f70ea62592b7c60e02300c18780087e3f19b2d
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-03-16 14:32:39 +0600

    add middleware to verify JWT access tokens

--- index.js
+++ index.js
@@ -78,6 +78,27 @@ const generateToken = (user) => {
   return { accessToken, refreshToken };
 };
 
+/*
+-------------------------
+Verify Access Token
+-------------------------
+*/
+
+const verifyAccessToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) return res.sendStatus(401);
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+
+    req.user = user;
+    
+    next();
+  });
+};
 async function run() {
   try {
     // Connect the client to the server
